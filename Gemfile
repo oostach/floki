ed6cdc8f4e4e362aa8e493fef1c6aa262c653fd3
oostach <oleksandr.ostach@gmail.com>
2023-02-02 09:06:04 +0200

    Move mini_profile badge to bottom-right corner

--- Gemfile
+++ Gemfile
@@ -33,7 +33,7 @@ end
 
 group :development do
   gem "web-console"
-  gem "rack-mini-profiler"
+  gem "rack-mini-profiler", require: false
   gem 'rubocop-rails'
   gem 'rubocop-rspec'
   gem 'letter_opener'
--- config/initializers/rack_profiler.rb
+++ config/initializers/rack_profiler.rb
@@ -3,6 +3,10 @@
 if Rails.env.development?
   require 'rack-mini-profiler'
 
+  Rack::MiniProfiler.config.tap do |config|
+    config.position = 'bottom-right'
+  end
+
   # initialization is skipped so trigger it
   Rack::MiniProfilerRails.initialize!(Rails.application)
 end
