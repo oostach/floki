35379de3b87b6212a8f6992ec261a396c4701f2e
oostach <oleksandr.ostach@gmail.com>
2023-02-06 23:50:20 +0200

    Add svg icon helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,29 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def svg_icon(name, path: nil, classes: '', options: {})
+    svg_file = find_svg_file(name, path)
+
+    body   = svg_file.to_s.force_encoding('UTF-8')
+    doc    = Nokogiri::HTML::DocumentFragment.parse body
+    svg    = doc.at_css 'svg'
+
+    update_icon_attributes(svg, classes:, options:)
+    raw doc # rubocop:disable Rails/OutputSafety
+  end
+
+  def update_icon_attributes(svg, classes:, options:)
+    svg['class'] = classes
+    options.each { |k, v| svg[k] = v }
+  end
+
+  def find_svg_file(name, path)
+    svg_path      = path || 'icons/svg'
+    svg_file_path = File.join(svg_path, "#{name}.svg")
+    svg_file      = AssetsDetector.find_assets_source(svg_file_path)
+
+    raise "Can't find #{svg_file_path}" unless svg_file
+
+    svg_file
+  end
 end
