27c089ccf856846c3607289fb6161244d457b450
oostach <oleksandr.ostach@gmail.com>
2023-02-14 00:25:45 +0200

    Close subscription form when click outside it

---
+++ app/javascript/controllers/subscriptions/form_controller.js
@@ -0,0 +1,21 @@
+import { Controller } from "@hotwired/stimulus"
+
+// Connects to data-controller="subscriptions--form"
+export default class extends Controller {
+  connect() {
+  }
+
+  hide(event) {
+    if (this.#is_clicked_on_element(event) || this.#is_clicked_inside_element(event)) return;
+
+    this.element.remove();
+  }
+
+  #is_clicked_inside_element(event) {
+    return this.element.contains(event.target);
+  }
+
+  #is_clicked_on_element(event) {
+    return this.element === event.target;
+  }
+}
