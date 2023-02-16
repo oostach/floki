91b77e8a11fa58983c4be180f6d9aa3679a89247
oostach <oleksandr.ostach@gmail.com>
2023-02-16 22:57:04 +0200

    Add a streamable news feed

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
 class DashboardController < ApplicationController
-  def show; end
+  def show
+    @posts = Post.all
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,6 @@ class Post < ApplicationRecord
   has_rich_text :content
 
   validates :title, :author, :source_url, presence: true
+
+  after_create_commit -> { broadcast_prepend_to 'dashboard_posts', partial: 'posts/post', locals: { post: self }, target: 'news-feed' }
 end
