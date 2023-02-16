c84580f60233739443c4d82f10ae5bb7c467f88b
oostach <oleksandr.ostach@gmail.com>
2023-02-16 17:18:56 +0200

    Fix rendering errors for the correct field

--- config/initializers/form_errors.rb
+++ config/initializers/form_errors.rb
@@ -5,25 +5,27 @@ ActionView::Base.field_error_proc = proc do |html_tag, instance|
   when  /\A<label\s/
     html_tag
   when  /\A<input\s/
-    FormErrorsWrapper.new(self, html_tag, instance.object).render
+    method_name = instance.instance_variable_get('@method_name')
+    FormErrorsWrapper.new(self, html_tag, instance.object, method_name).render
   else
     content_tag :div, html_tag, class: 'field_with_errors'
   end
 end
 
 class FormErrorsWrapper
-  attr_reader :context, :object, :html_tag
+  attr_reader :context, :object, :html_tag, :method_name
 
-  def initialize(context, html_tag, object)
-    @context  = context
-    @html_tag = html_tag
-    @object   = object
+  def initialize(context, html_tag, object, method_name)
+    @context     = context
+    @html_tag    = html_tag
+    @object      = object
+    @method_name = method_name
   end
 
   def render
     context.content_tag :div, class: 'field-with-errors' do
       html_tag +
-        context.content_tag(:span, object.errors.full_messages_for(:title).join(', '), class: 'field-errors')
+        context.content_tag(:span, object.errors.full_messages_for(method_name).join(', '), class: 'field-errors')
     end
   end
 end
