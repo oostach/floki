9a3c8fb2777e20896844a81680b9e77ece92e0de
oostach <oleksandr.ostach@gmail.com>
2023-02-22 00:15:58 +0200

    Add github actions to run tests

--- spec/system/dashboard_spec.rb
+++ spec/system/dashboard_spec.rb
@@ -5,6 +5,6 @@ require 'rails_helper'
 RSpec.describe 'Dashboard page' do
   it 'enables me to see dashboard' do
     visit '/'
-    expect(page).to have_text('Dashboard#show')
+    expect(page).to have_text('News Feed')
   end
 end
