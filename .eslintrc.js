efeb6666feba1e72d81d1ff21ba1922d1571d60a
oostach <oleksandr.ostach@gmail.com>
2023-02-21 13:13:37 +0200

    Add config for the ESLint linter

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    'jest/globals': true
+  },
+  extends: 'standard',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: ['jest'],
+  globals: {
+    ZoomMtg: 'readable'
+  },
+  rules: {
+    'space-before-function-paren': 'off',
+    'multiline-ternary': 'off',
+    'n/no-callback-literal': 'off',
+    'accessor-pairs': 'off',
+    'no-prototype-builtins': 'off',
+    'no-return-assign': 'off'
+  }
+}
