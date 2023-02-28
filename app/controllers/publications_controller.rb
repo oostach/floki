34ceddedbdd669e3a3e6e14a48084dea1b6a587c
oostach <oleksandr.ostach@gmail.com>
2023-02-28 20:14:51 +0200

    Update and destroy publication

--- app/controllers/publications_controller.rb
+++ app/controllers/publications_controller.rb
@@ -1,13 +1,23 @@
 # frozen_string_literal: true
 
 class PublicationsController < ApplicationController
+  PREPAGE = 5
+
+  before_action :load_publication, only: %i[show edit destroy update]
+
   def index
     @publications = Publication.all
+  end
+
+  def show
+    render @publication
   end
 
   def new
     @publication = Publication.new
   end
+
+  def edit; end
 
   def create
     @publication = Publication.new(publication_params)
@@ -20,9 +30,26 @@ class PublicationsController < ApplicationController
     end
   end
 
+  def update
+    if @publication.update(publication_params)
+      render @publication
+    else
+      render action: :edit
+    end
+  end
+
+  def destroy
+    @publication.destroy
+    flash.now.notice = 'Your note has been successfully removed.'
+  end
+
   private
 
   def publication_params
     params.require(:publication).permit(:title, :description, :url, :author, files: [])
+  end
+
+  def load_publication
+    @publication = Publication.find(params[:id])
   end
 end
