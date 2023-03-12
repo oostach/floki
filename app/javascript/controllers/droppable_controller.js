77b015102ef6a6b3c80fb725ff17d8a834ff9b7f
oostach <oleksandr.ostach@gmail.com>
2023-03-13 01:52:13 +0200

    Preview images without uploading

--- app/javascript/controllers/droppable_controller.js
+++ app/javascript/controllers/droppable_controller.js
@@ -3,32 +3,20 @@ import { Turbo } from '@hotwired/turbo-rails'
 
 // Connects to data-controller="flash-messages"
 export default class extends Controller {
-  static targets = ['droppableArea', 'filesField']
+  static targets = ['droppableArea', 'filesField', 'imagesPreviewArea']
+
+  static values = {
+    resourcesUrl: String,
+    imageTemplate: String,
+    uploadable: Boolean
+  }
 
   uploadOrPreviewFiles(e) {
     e.preventDefault()
 
-    const filesData = new FormData()
     const files = e.dataTransfer.files
 
-    for (const file of files) {
-      filesData.append('publication[files][]', file)
-    }
-
-    fetch(this.#addResourcesUrl, {
-      method: 'POST',
-      mode: 'same-origin',
-      credentials: 'same-origin',
-      headers: {
-        'X-CSRF-Token': this.#csrfToken,
-        Accept: 'text/vnd.turbo-stream.html'
-      },
-      body: filesData
-    }).then(res => res.text())
-      .then(html => {
-        Turbo.renderStreamMessage(html)
-        this.#buildFormFields(files)
-      })
+    this.uploadableValue ? this.#uploadFiles(files) : this.#previewFiles(files)
   }
 
   highlightDroppableArea(e) {
@@ -45,11 +33,55 @@ export default class extends Controller {
     return document.getElementsByName('csrf-token')[0].content
   }
 
-  get #addResourcesUrl() {
-    return this.data.element.dataset.addResourcesUrl
+  #buildFormFields(files) {
+    const fileField = document.createElement('input')
+    fileField.setAttribute('type', 'file')
+    fileField.setAttribute('name', 'publication[files][]')
+    fileField.setAttribute('class', 'hidden')
+    fileField.files = files
+    this.element.appendChild(fileField)
   }
 
-  #buildFormFields(files) {
-    this.filesFieldTarget.files = files
+  #previewFiles(files) {
+    for (const file of files) {
+      file.type.match(/^image\//)
+        ? this.#previewImage(file)
+        : this.#previewFile(file)
+    }
+    this.#buildFormFields(files)
+  }
+
+  #previewImage(file) {
+    const parser = new DOMParser()
+    const htmlDoc = parser.parseFromString(this.imageTemplateValue, 'text/html')
+    htmlDoc.querySelector('.image-name').innerText = file.name
+    htmlDoc.querySelector('.image-preview').src = URL.createObjectURL(file)
+    this.imagesPreviewAreaTarget.append(htmlDoc.querySelector('.image'))
+  }
+
+  #previewFile() {
+
+  }
+
+  #uploadFiles(files) {
+    const filesData = new FormData()
+
+    for (const file of files) {
+      filesData.append('publication[files][]', file)
+    }
+
+    fetch(this.resourcesUrlValue, {
+      method: 'POST',
+      mode: 'same-origin',
+      credentials: 'same-origin',
+      headers: {
+        'X-CSRF-Token': this.#csrfToken,
+        Accept: 'text/vnd.turbo-stream.html'
+      },
+      body: filesData
+    }).then(res => res.text())
+      .then(html => {
+        Turbo.renderStreamMessage(html)
+      })
   }
 }
