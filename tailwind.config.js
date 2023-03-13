5d49de7cc81698fa7f95cceb6d9494479d54e493
oostach <oleksandr.ostach@gmail.com>
2023-03-14 01:26:30 +0200

    Add css icons instead of images

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,10 +10,20 @@ module.exports = {
   ],
   safelist: [
     { pattern: /flash-message-(notice|alert|info)/ },
-    { pattern: /invisible|hidden|field-with-errors|required|pagination-button|pagination|form-richtext/ }
+    { pattern: /invisible|hidden|field-with-errors|required|pagination-button|pagination|form-richtext/ },
+    { pattern: /bg-icon-(zip|png|pdf|jpg|doc|html|mov)/ }
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'icon-png': 'url("./icons/png/file-types/png.png")',
+        'icon-html': 'url("./icons/png/file-types/html.png")',
+        'icon-doc': 'url("./icons/png/file-types/doc.png")',
+        'icon-pdf': 'url("./icons/png/file-types/pdf.png")',
+        'icon-zip': 'url("./icons/png/file-types/zip.png")',
+        'icon-jpg': 'url("./icons/png/file-types/jpg.png")',
+        'icon-mov': 'url("./icons/png/file-types/mov.png")'
+      },
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
         opensans: ['"Open Sans"', ...defaultTheme.fontFamily.sans]
