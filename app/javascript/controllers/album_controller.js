790740560e786cc7ac27a71505a2d70b6ad6aa4c
oostach <oleksandr.ostach@gmail.com>
2023-03-19 01:48:14 +0200

    Improve the album close button

--- app/javascript/controllers/album_controller.js
+++ app/javascript/controllers/album_controller.js
@@ -7,7 +7,11 @@ export default class extends Controller {
   static template = `
     <div class='album' data-album-target='container' style='display: none;'>
       <div class='album-header'>
-        <dvi class='album-close-btn' data-action='click->album#closeAlbum'>X</dvi>
+        <dvi class='album-close-btn' data-action='click->album#closeAlbum'>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12"/>
+          </svg>
+        </dvi>
       </div>
       <div class='album-body'>
         <div class='album-image' data-album-target='image'></div>
