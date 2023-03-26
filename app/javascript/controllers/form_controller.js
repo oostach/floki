00347a16536da545e260d7c6355c81cc7bd8f67f
oostach <oleksandr.ostach@gmail.com>
2023-03-26 16:55:53 +0300

    Move stimulus form controller to the general scope

--- app/javascript/controllers/form_controller.js
+++ app/javascript/controllers/form_controller.js
@@ -2,7 +2,7 @@
 
 import { Controller } from '@hotwired/stimulus'
 
-// Connects to data-controller="notes--form"
+// Connects to data-controller="form"
 export default class extends Controller {
   connect() {
   }
--- app/javascript/controllers/form_controller.test.js
+++ app/javascript/controllers/form_controller.test.js
@@ -10,7 +10,7 @@ import FormController from './form_controller'
 describe('FormController', () => {
   beforeEach(() => {
     document.body.innerHTML = `
-      <div class="notes-form" data-controller="notes--form">
+      <div class="form" data-controller="form">
         <form action="/notes" accept-charset="UTF-8" method="post">
           <input type="hidden" name="authenticity_token"
             value="GlBfPm4l44y6ck0k3N6nLKt-mlUH929IVf6ilTTSp6Q-e9x2KKREd4jnm5wbpYMb7K_dRfC5d6IGMOSz7tfYMQ" autocomplete="off">
@@ -24,17 +24,17 @@ describe('FormController', () => {
           </div>
           <div class="form-group">
             <input type="submit" name="commit" value="Create Note" class="button-primary" data-disable-with="Create Note">
-            <a class="button-secondary button-cancel" data-action="click->notes--form#cancel" href="#">Cancel</a>
+            <a class="button-secondary button-cancel" data-action="click->form#cancel" href="#">Cancel</a>
           </div>
         </form>
         <hr class="my-3 -mx-4 border-gray-300">
       </div>
     `
-    StimulusApp.start().register('notes--form', FormController)
+    StimulusApp.start().register('form', FormController)
   })
 
   test('it should remove form on click cancel', () => {
-    const form = document.querySelector('.notes-form')
+    const form = document.querySelector('.form')
     const cancelButton = form.querySelector('.button-cancel')
     cancelButton.click()
 
