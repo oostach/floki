50282a8acded2fa0ac7a7dcb98c117955520bd3b
oostach <oleksandr.ostach@gmail.com>
2023-04-07 10:40:34 +0300

    Move custom form builder to a separate gem

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| 'https://github.com/#{repo}.git' }
 
 ruby '3.2.0'
 
-gem 'rails', '~> 7.0.4', '>= 7.0.4.2'
+gem 'rails', '~> 7.0.4', '>= 7.0.4.3'
 gem 'sprockets-rails'
 gem 'pg', '~> 1.1'
 gem 'puma', '~> 5.0'
@@ -25,6 +25,7 @@ gem 'image_processing', '~> 1.2'
 # gem 'telegram-bot'
 gem 'octokit'
 gem 'faraday-retry'
+gem 'floki-form', path: '~/projects/floki-form/'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- app/helpers/floki_form_helper.rb
+++ app/helpers/floki_form_helper.rb
@@ -3,12 +3,12 @@
 module FlokiFormHelper
   def floki_form_for(name, *args, &)
     options = args.extract_options!
-    args << options.merge(builder: FlokiFormBuilder)
+    args << options.merge(builder: FlokiForm::FormBuilder)
     form_for(name, *args, &)
   end
 
   def floki_form_with(model: nil, scope: nil, url: nil, format: nil, **options, &)
-    options = options.reverse_merge(builder: FlokiFormBuilder)
+    options = options.reverse_merge(builder: FlokiForm::FormBuilder)
     form_with(model: model, scope: scope, url: url, format: format, **options, &)
   end
 end
