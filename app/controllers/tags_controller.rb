d896c7800e5c3ce5d5d7786688d8a94e1dd25830
oostach <oleksandr.ostach@gmail.com>
2023-04-18 23:16:05 +0300

    Add owner to the tags form

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -7,9 +7,20 @@ class TagsController < ApplicationController
     @tags = @owner.tags.list
   end
 
+  def create
+  end
+
   private
 
   def load_owner
-    @owner = GlobalID::Locator.locate params[:owner]
+    @owner = GlobalID::Locator.locate owner_param
+  end
+
+  def tags_params
+    params.require(:tags).permit(:name, :owner)
+  end
+
+  def owner_param
+    params[:owner] || tags_params[:owner]
   end
 end
