9d405b9d6fde20a6823ed3e393c2a9aad19e6207
oostach <oleksandr.ostach@gmail.com>
2023-04-20 01:28:18 +0300

    Update tags filter on adding a tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,6 @@ class Tag < ApplicationRecord
   has_many :tag_mappings, inverse_of: :tag, dependent: :destroy
 
   validates :name, uniqueness: { scope: :owner_class }
+
+  scope :for_owner_class, ->(owner) { joins(:tag_mappings).where(owner_class: owner.class.name) }
 end
