dc359d2222d6b73c9b6946d25fd0d69cc01b1827
oostach <oleksandr.ostach@gmail.com>
2023-05-02 21:57:52 +0300

    Close the edit dialog by clicking outside the form

--- app/javascript/components/todos/edit-form.jsx
+++ app/javascript/components/todos/edit-form.jsx
@@ -13,8 +13,11 @@ const TodoEditForm = ({ currentItem, updateItem, disableEditMode }) => {
   }
 
   return (
-    <div className='edit-wrapper absolute z-10 bg-gray-200 bg-opacity-90 top-0 bottom-0 left-0 right-0 h-screen'>
-      <form className='todo-edit-form mx-auto mt-[50vh] w-1/2' onSubmit={handleFormSubmit}>
+    <div
+      className='edit-wrapper absolute z-10 bg-gray-200 bg-opacity-90 bg-blend-overlay top-0 bottom-0 left-0 right-0 h-screen'
+      onClick={(e) => { return e.target === e.currentTarget && disableEditMode() }}
+      >
+      <form className='todo-edit-form mx-auto mt-[40vh] w-1/2 p-4 border-2 border-sky-500 bg-sky-100 rounded-lg shadow-md shadow-sky-500/50' onSubmit={handleFormSubmit}>
         <div className='form-group flex'>
           <input type='text' className='form-input mr-2' value={editedItemTitle} onInput={(e) => setEditedItemTitle(e.target.value)} required autoFocus />
           <button className='button-primary mr-2' aria-label='Update Task' type='submit'>Update</button>
