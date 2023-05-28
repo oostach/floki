374efcedca093332e13e641c05ea381d395a0e4b
oostach <oleksandr.ostach@gmail.com>
2023-05-28 22:32:22 +0300

    Add sorting by date

--- app/assets/javascripts/components/todos/sections/filter.jsx
+++ app/assets/javascripts/components/todos/sections/filter.jsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
-const TodosFilter = ({ filterCompleted }) => {
+import { ArrowDownIcon, ArrowUpIcon } from '@heroicons/react/24/outline'
+
+const TodosFilter = ({ filterCompleted, sortTodos }) => {
   const [active, setActive] = useState(false)
+  const [quickest, setQuickest] = useState('unsorted')
 
   const toggleHidden = (e) => {
     e.preventDefault()
@@ -10,23 +13,44 @@ const TodosFilter = ({ filterCompleted }) => {
     filterCompleted(!active)
   }
 
-  const classList = () => {
-    const list = ['todos-filter', 'ml-auto', 'flex', 'content-center', 'flex-wrap', 'text-sm', 'text-sky-700']
-    if (active) {
+  const sortByDue = (e) => {
+    e.preventDefault()
+
+    if (quickest === 'latest') {
+      setQuickest(prevQuickest => 'recent')
+      sortTodos('recent')
+    } else if (quickest === 'recent') {
+      setQuickest(prevQuickest => 'unsorted')
+      sortTodos('unsorted')
+    } else {
+      setQuickest(prevQuickest => 'latest')
+      sortTodos('latest')
+    }
+  }
+
+  const classList = (enabled) => {
+    const list = []
+    if (enabled) {
       list.push('underline', 'font-semibold')
     }
     return list.join(' ')
   }
 
   return (
-    <div className={classList()}>
-      <a href='#' onClick={toggleHidden}>Non Completed Only</a>
+    <div className='todos-filter ml-auto flex content-center flex-wrap text-sm text-sky-700'>
+      <a href='#' onClick={sortByDue} className={'mr-3 ' + classList(quickest !== 'unsorted')}>
+        { quickest === 'latest' && <ArrowDownIcon width={'16px'} className='inline-block mr-1' /> }
+        { quickest === 'recent' && <ArrowUpIcon width={'16px'} className='inline-block mr-1' /> }
+        Sort by Date
+      </a>
+      <a href='#' onClick={toggleHidden} className={classList(active)}>Non Completed Only</a>
     </div>
   )
 }
 
 TodosFilter.propTypes = {
-  filterCompleted: PropTypes.func
+  filterCompleted: PropTypes.func,
+  sortTodos: PropTypes.func
 }
 
 export default TodosFilter
--- app/assets/javascripts/components/todos/sections/list.jsx
+++ app/assets/javascripts/components/todos/sections/list.jsx
@@ -11,6 +11,7 @@ import { handleDragover, handleDrop, handleDragEnd } from '../utils/draggable'
 
 const TodoList = ({ items, list, enableEditMode }) => {
   const [hidden, setHidden] = useState(false)
+  const [sort, setSort] = useState('unsorted')
 
   const [updatePosition] = useMutation(UPDATE_POSITION, {
     update(cache, { data }) {
@@ -26,14 +27,24 @@ const TodoList = ({ items, list, enableEditMode }) => {
 
   const filterCompleted = hiddenState => setHidden(hiddenState)
 
+  const sortedItems = () => {
+    if (sort === 'latest') {
+      return items.sort((a, b) => new Date(a.dueDate) - new Date(b.dueDate))
+    } else if (sort === 'recent') {
+      return items.sort((a, b) => new Date(b.dueDate) - new Date(a.dueDate))
+    } else {
+      return items.sort((a, b) => a.position - b.position)
+    }
+  }
+
   return (
     <div className='todos-list mt-4 pb-2' onDragOver={handleDragover} onDrop={(e) => handleDrop(e, updatePosition, { listId: list.id })} onDragEnd={handleDragEnd}>
       <div className='flex content-center mb-2 flex-wrap'>
         <div className='todos-list-name font-semibold text-lg text-sky-900'>{list.name}</div>
-        <TodosFilter filterCompleted={filterCompleted} />
+        <TodosFilter filterCompleted={filterCompleted} sortTodos={(state) => setSort(prevSort => state)} />
       </div>
       {
-        items.map(item => {
+        sortedItems().map(item => {
           return <TodoItem key={item.id} item={item} listId={list.id} enableEditMode={enableEditMode} hidden={item.completed && hidden} />
         })
       }
