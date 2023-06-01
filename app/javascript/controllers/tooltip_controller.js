b01cbd8e5df91cd91e16e06d764de405c7c56718
oostach <oleksandr.ostach@gmail.com>
2023-06-01 17:21:56 +0300

    Add the left placement to the tooltip

--- app/javascript/controllers/tooltip_controller.js
+++ app/javascript/controllers/tooltip_controller.js
@@ -27,47 +27,54 @@ export default class extends Controller {
     return tooltipWrapper
   }
 
-  #leftPlacementOffset() {
-
-  }
-
-  #rightPlacementOffset() {
-
-  }
-
-  #bottomPlacementOffset() {
-
-  }
-
-  #topPlacementOffset() {
-    const elementSize = this.element.getBoundingClientRect()
-    const tooltipSize = this.#tooltip.getBoundingClientRect()
-
+  #leftPlacementOffset(elementSize, tooltipSize) {
     return {
-      x: elementSize.top - tooltipSize.height + window.scrollY - 5 + 'px',
-      y: elementSize.left + (elementSize.width - tooltipSize.width) / 2 + 'px'
+      x: Math.round(elementSize.left - tooltipSize.width - 5) + 'px',
+      y: Math.round(elementSize.top + window.scrollY + (elementSize.height - tooltipSize.height) / 2) + 'px'
+    }
+  }
+
+  #rightPlacementOffset(elementSize, tooltipSize) {
+
+  }
+
+  #bottomPlacementOffset(elementSize, tooltipSize) {
+    return {
+      x: Math.round(elementSize.top - tooltipSize.height + window.scrollY - 5) + 'px',
+      y: Math.round(elementSize.left + (elementSize.width - tooltipSize.width) / 2) + 'px'
+    }
+  }
+
+  #topPlacementOffset(elementSize, tooltipSize) {
+    return {
+      x: elementSize.left + (elementSize.width - tooltipSize.width) / 2 + 'px',
+      y: elementSize.top - tooltipSize.height + window.scrollY - 5 + 'px'
     }
   }
 
   #tooltipOffsets() {
+    const elementSize = this.element.getBoundingClientRect()
+    const tooltipSize = this.#tooltip.getBoundingClientRect()
+
     switch (this.placementValue) {
       case 'left':
-        return this.#leftPlacementOffset()
+        return this.#leftPlacementOffset(elementSize, tooltipSize)
       case 'right':
-        return this.#rightPlacementOffset()
+        return this.#rightPlacementOffset(elementSize, tooltipSize)
       case 'bottom':
-        return this.#bottomPlacementOffset()
+        return this.#bottomPlacementOffset(elementSize, tooltipSize)
       default:
-        return this.#topPlacementOffset()
+        return this.#topPlacementOffset(elementSize, tooltipSize)
     }
   }
 
   show() {
     this.#tooltip = this.#buildTooltip()
     this.element.before(this.#tooltip)
+
     const tooltipOffsets = this.#tooltipOffsets()
-    this.#tooltip.style.top = tooltipOffsets.x
-    this.#tooltip.style.left = tooltipOffsets.y
+    this.#tooltip.style.top = tooltipOffsets.y
+    this.#tooltip.style.left = tooltipOffsets.x
   }
 
   hide() {
