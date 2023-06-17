fd3af9db0013f623cefb54d079fafff7b6a3d41b
oostach <oleksandr.ostach@gmail.com>
2023-06-17 17:06:06 +0300

    Add some stuff to the SyncRepositoryJob

--- app/jobs/sync_repository_job.rb
+++ app/jobs/sync_repository_job.rb
@@ -3,11 +3,23 @@
 class SyncRepositoryJob < ApplicationJob
   queue_as :default
 
+  attr_reader :repository
+
   # rescue_from(ActiveRecord::RecordNotFound) do |exception|
   # end
 
   def perform(id)
-    repository = Repository.find(id)
-    repository.update(name: SecureRandom.alphanumeric(20))
+    @repository = Repository.find(id)
+    @repository.update(name: SecureRandom.alphanumeric(20))
+  end
+
+  private
+
+  def client
+    @client ||= Octokit::Client.new
+  end
+
+  def repo
+    @repo ||= client.repo @repository.repo_short_path
   end
 end
